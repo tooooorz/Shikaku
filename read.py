78c6b6f145c1111524ce001b7c0946cc57297d86
tooooorz <mikankanmi@icloud.com>
2026-08-18 17:34:37 +0900

    read_problem関数にて勝手にオフライン変数のReadingLineが使用されることを修正。my_listの中をstr型で読み取ってしまうのを修正。他諸々修正。

--- read.py
+++ read.py
@@ -5,33 +5,47 @@ w,h = 0,0
 my_list = []
 board = []
 
-def If_naturalnumber():
+is_natural = all(
+    isinstance(x, int) 
+    and not isinstance(x, bool) 
+    and x > 0 for x in my_list
+    )
+
+xs = my_list[1::2]
+ys = my_list[::2]
+is_inside_board = (
+    all(x <= h for x in ys) 
+    and all(x <= w for x in xs)
+)
+
+def problemToList():
     if ReadingLine == 1 and len(my_list) == 2:
-        if all(isinstance(x, int) and not isinstance(x, bool) and x > 0 for x in my_list): #自然数かどうか判定
-            w = my_list[1], h = my_list[0]
-            row = [0 for _ in range(w)]
+        if is_natural: #自然数かどうか判定
+            w = my_list[1]; h = my_list[0]
             for _ in range(h):
-                board.append(row)
-        else: print("This problem is invalid"); sys.exit()
+                board.append([0 for _ in range(w)])
+        else: print("This problem is invalid"); print(ReadingLine); sys.exit()
     elif ReadingLine >= 2 and len(my_list) == 6:
-        if all(isinstance(x, int) and not isinstance(x, bool) and x > 0 for x in my_list):
-            if all(x <= h for x in my_list[::2]) and all(x <= w for x in my_list[1::2]):
-                for i in range(my_list[4]):
-                    for j in range(my_list[3]):
-                        board_x = my_list[1] - 1 + i
-                        board_y = my_list[0] - 1 + j
-                        board[board_x][board_y] = 0
-            board[my_list[5]][my_list[4]] = my_list[2] * my_list[3]
-        else: print("This problem is invalid"); sys.exit()
-    else: print("This problem is invalid"); sys.exit()
+        if is_natural and is_inside_board:
+            for i in range(my_list[3]):
+                for j in range(my_list[2]):
+                    board_x = my_list[1] - 1 + i
+                    board_y = my_list[0] - 1 + j
+                    print(ReadingLine)
+                    print(board_x, board_y)
+                    board[board_x][board_y] = 0
+        else: print("This problem is invalid"); print(ReadingLine); sys.exit()
+        board[my_list[4]-1][my_list[5]-1] = my_list[2] * my_list[3]
+        print(board)
+    else: print("This problem is invalid"); print(ReadingLine); sys.exit()
 
 def read_problem():
     with open("./problem/1", "r", encoding="utf-8") as f:
         for line in f:
+            global ReadingLine, my_list
             ReadingLine += 1
-            my_list = line.split()
-            If_naturalnumber()
+            my_list = [int(x) for x in line.split()]
+            problemToList()
 
 read_problem()
 print(board)
-
