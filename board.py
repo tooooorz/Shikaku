8800dc34c5c2a8c5afca3f1cfdc8ca30c35380c6
tooooorz <mikankanmi@icloud.com>
2026-08-23 00:14:19 +0900

    レイヤー機能実装

--- board.py
+++ board.py
@@ -2,10 +2,13 @@ import tkinter as tk
 
 def CheckSquareType(w,h):
     if w == h:
+        canvas.create_rectangle(100, 100, 900, 900, fill="#f5f5dc", tags="background")
         SquareTypeA(w,h) #正方形
     elif w > h:
+        canvas.create_rectangle(100, 500-400/w*h, 900, 500+400/w*h, fill="#f5f5dc", tags="background")
         SquareTypeB(w,h) #長方形(横長)
     elif w < h:
+        canvas.create_rectangle(500-400/h*w, 100, 500+400/h*w, 900, fill="#f5f5dc", tags="background")
         SquareTypeC(w,h) #長方形(縦長)
     else:
         print("error") 
@@ -17,7 +20,7 @@ def SquareTypeA(w,h):
             y1 = 100+800/h*j
             x2 = x1 + 800/w
             y2 = y1 + 800/h
-            canvas.create_rectangle(x1, y1, x2, y2, fill="#f5f5dc", width=5)
+            canvas.create_rectangle(x1, y1, x2, y2, width=5, tags="square")
 
 def SquareTypeB(w,h):
     for i in range(w):
@@ -26,7 +29,7 @@ def SquareTypeB(w,h):
             y1 = 500-400/w*h+800/w*j
             x2 = x1 + 800/w
             y2 = y1 + 800/w
-            canvas.create_rectangle(x1, y1, x2, y2, fill="#f5f5dc", width=5)
+            canvas.create_rectangle(x1, y1, x2, y2, width=5, tags="square")
 
 def SquareTypeC(w,h):
     for i in range(w):
@@ -35,14 +38,14 @@ def SquareTypeC(w,h):
             y1 = 100+800/h*j
             x2 = x1 + 800/h
             y2 = y1 + 800/h
-            canvas.create_rectangle(x1, y1, x2, y2, fill="#f5f5dc", width=5)
+            canvas.create_rectangle(x1, y1, x2, y2, width=5, tags="square")
 
 root = tk.Tk()
 root.geometry("1000x1000")
 
 canvas = tk.Canvas(root, bg="#ffffff", height=1000, width=1000)
 
-CheckSquareType(5,8)
+CheckSquareType(8,5)
 
 canvas.pack()
 tk.mainloop()
