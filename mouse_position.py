2741882772b03ad0ac4f4daaf46a874750f85ce4
tooooorz <mikankanmi@icloud.com>
2026-08-23 19:27:13 +0900

    リアルタイムで四角を描画

--- mouse_position.py
+++ mouse_position.py
@@ -1,20 +1,18 @@
 import tkinter as tk
 
 def mouse_start(event):
-    global start_x, start_y
+    global start_x, start_y, provisinal_rectangle
     start_x = event.x; start_y = event.y
+    provisinal_rectangle = canvas.create_rectangle(start_x, start_y, start_x, start_y, fill="#000000")
     print("開始:", event.x, event.y)
 
 def mouse_drag(event):
+    canvas.coords(provisinal_rectangle, start_x, start_y, event.x, event.y)
     print("ドラッグ中:", event.x, event.y)
 
 def mouse_end(event):
-    global end_x, end_y
-    end_x = event.x; end_y = event.y
+    canvas.coords(provisinal_rectangle, start_x, start_y, event.x, event.y)
     print("終了:", event.x, event.y)
-
-def rectangle():
-    canvas.create_rectangle(start_x, start_y, end_x, end_y, fill="#000000")
 
 root = tk.Tk()
 
@@ -23,6 +21,6 @@ canvas.pack()
 
 canvas.bind("<ButtonPress-1>", mouse_start)
 canvas.bind("<B1-Motion>", mouse_drag)
-canvas.bind("<ButtonRelease-1>", mouse_end, rectangle)
+canvas.bind("<ButtonRelease-1>", mouse_end)
 
 root.mainloop()
